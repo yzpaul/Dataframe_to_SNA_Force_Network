605838838ac112bc577b91bd0d0bcb74b85255a8
yzpaul <yzpaul@gmail.com>
2019-10-06 01:27:42 -0400

    massive readability rewrite

--- dynamic_network_standalone_method.R
+++ dynamic_network_standalone_method.R
@@ -1,66 +1,65 @@
-# library(igraph)
-# library(networkD3)
-# csv<-read.csv("C:\\Users\\King\\Desktop\\campbell\\minecraftdatapuller\\Analysis\\minecraft_with_0_and_MI.csv")
-# dtm2<-data.frame(csv$stat.damageDealt,csv$stat.deaths,csv$stat.mob)#damage delt on deaths, mob kills (is color)
-# #write.csv(dtm2,"output_df_for_jason.csv",row.names=F)
-# #adding UNIQUE names column
-# dtm2$names<-1:length(dtm2[,1]) 
-# dtm2$names<-paste("uniqueID_",dtm2$names,sep="")
-# 
-# dtm2$colors <- ifelse(is.na(dtm2[,3]), 999, 
-#                  ifelse(as.numeric(dtm2[,3]) < 10, 1, 
-#                         ifelse(as.numeric(dtm2[,3]) >= 30, 3, 2)))
-# #reorder columns so unique id is next to kills. uniqueID will be source and target will be kills 
-# dtm2<-dtm2[,c(4,2,5,1)]
-# #add lineweights to dataframe
-# dtm2$lineWeights<-3
-# #run function and display forceNetwork in browser
-# res<-forceNetwork_from_dataframe(dtm2)
-# htmltools::html_print(res, viewer = utils::browseURL)
+library(igraph)
+library(networkD3)
 
-####################################################################################################################################
+dtm2<-data.frame("source"=c(1,2,3,4,5,6,7,8,9),
+                 "target"=c(8,2,8,8,1,5,4,3,7))
 
-#dataframe col1=source(aka unique id). col2=target to link source to. col3=color atttribute. col4=vertex labels, col5=line weights
-#d3 color group order: blue, orange, green, red
-forceNetwork_from_dataframe<-function(dtm2){
-set.seed(42)
-g<-graph.data.frame(dtm2[,1:ncol(dtm2)], directed=TRUE) #directed=TRUE because one unique_id (aka source) only points to one target
-V(g)$type <- V(g)$name %in% dtm2[,1] #purpose is to give true and false for node and community?????
+#This graph assumes the link value is 1 by default. You can change it here.
+#In the example graph below it is used to set the line width
+dtm2$val<-1
 
-i<-table(V(g)$type)[2]#captures number of unique actors in dtm2.... is 15
-j<-table(V(g)$type)[1]#gets number of communities in dtm2
+#this graph assumes none of the nodes are grouped into communites, but you can specify groupings here:
+dtm2$community<-1:dim(dtm2)[1]
 
+####################################################################################
+# 1) Generate basic graph object:
+####################################################################################
+  g<-graph.data.frame(dtm2[,1:ncol(dtm2)], directed=TRUE) #directed=TRUE because one unique_id (aka source) only points to one target
+  V(g)$type <- V(g)$name %in% dtm2[,1] #true if node, false if community
 
-V(g)$name<-1:length(V(g)) #numbers each node as unique number. is source for links
-V(g)$label<-paste("name:",V(g)$name,"_dmgDealt:",dtm2[,4],sep="")
-V(g)$label[i+1:j]<-"community" #label communities as community. syntax when end num in : less then beginning do repeat for end number times
-#V(g)$label<-V(g)$name
+  numact<-table(V(g)$type)[2]#captures number of unique actors in dtm2
+  numcom<-table(V(g)$type)[1]#gets number of communities in dtm2
+####################################################################################
+# 2) Calculate node (vertex) labels:
+####################################################################################
+  V(g)$label<-V(g)$name #numbers each node as unique number. is source for links
+  
+  #make sure to set the lowest label value to 0. Because R starts indexes at 1, 
+  #but JavaScript (which actually builds the graph) starts them at 0
+  V(g)$label<-as.character(as.numeric(V(g)$label)-min(as.numeric(V(g)$label)))
+  
+  #V(g)$label[numact+1:numcom]<-"community" #to make some nodes communities
+  
+####################################################################################
+# 3) Count number of communities and actors: 
+####################################################################################
+  numact<-table(V(g)$type)[2]#captures number of unique actors that arent in community
+  numcom<-table(V(g)$type)[1]#gets number of communities in dtm2
 
-links<-as.data.frame(get.edgelist(g))
-links$V1<-as.numeric(as.character(links$V1))
-#links
+####################################################################################  
+# 4) generate list of links  
+####################################################################################
+  links<-as.data.frame(get.edgelist(g))
+  
+  #make sure to set the lowest label value to 0. R starts indexes at 1, 
+  #but JavaScript (which actually builds the graph) starts them at 0
+  links<-apply(links,2,as.numeric)
+  minlink<-min(links)
+  links<-as.data.frame(apply(links,2,function(x){x-minlink}))
+  
+  colnames(links)<-c("source","target")
+  links$value <- dtm2$val
 
-links$V2<-as.numeric(as.character(links$V2))
-#str(links)
+####################################################################################
+# 5) generate list of nodes
+####################################################################################
+  #if you get an error here make sure there are no duplicate source/target pairs in original data 
+  #then run this at start of your code:
+  #dtm2<-dtm2 %>% distinct(source, target, .keep_all = TRUE)
+  nodes <- data.frame(name=V(g)$label, group=dtm2$community)#label and color value of each node
 
-colnames(links)<-c("source","target")
-#links
-links<-(links-1)#can show all links (start count from 0)
-links$value <- dtm2[,5] 
-#links
-
-
-#add communities to color list.
-groupings<-c(dtm2[,3],rep(999,j))#j is number of communities
-
-#nodes <- data.frame(name=V(g)$label, group=c(rep(1,i),rep(2,(j))))#change group numbers####CLASS CODE
-nodes <- data.frame(name=V(g)$label, group=groupings)
-nodes #shows actors and group
-
-nothingbutnet<-forceNetwork(Links = links, Nodes = nodes,
-             Source = "source", Target = "target",
-             NodeID = "name", Value = "value",
-             Group = "group", opacity = 0.8, colourScale = "d3.scale.category10()")
-
-return(nothingbutnet)
-}
+####################################################################################  
+# 6) pass params into forcenetwork to complete example
+#################################################################################### 
+forceNetwork(Links = links, Nodes = nodes,Source = "source", Target = "target",
+          NodeID = "name", Value = "value",Group = "group", opacity = 0.8)
